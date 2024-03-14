6eaf17e9ff7a8691c0574d0bfb5505d9b5fe4c54
NGUYENDUONG5G <apeach2901@gmail.com>
2024-03-14 16:38:41 +0700

    hello world

---
+++ SDL_Game/ThreatsObject.h
@@ -0,0 +1,121 @@
+﻿#pragma once
+#pragma once
+#ifndef THREATS_OBJECT_H_
+#define THREATS_OBJECT_H_
+#include "CommonFunc.h"
+#include "BaseObject.h"
+#include "BulletObject.h"
+
+
+#define THREAT_FRAME_NUM 4
+#define THREAT_GRAVITY_SPEED 0.8
+#define THREAT_MAX_FALL_SPEED 10
+#define THREAT_SPEED 3
+
+
+
+class ThreatsObject :public BaseObject
+{
+public:
+	ThreatsObject();
+	~ThreatsObject();
+
+	enum TypeMove
+	{
+		STATIC_THREAT = 0,
+		MOVE_IN_SPACE_THREAT = 1,
+	};
+
+	void set_x_val(const float& xVal) {
+		x_val_ = xVal;
+	}
+
+	void set_y_val(const float& yVal) {
+		y_pos_ = yVal;
+	}
+
+	void set_x_pos(const float& xp) {
+		x_pos_ = xp;
+	}
+	void set_y_pos(const float& yp) {
+		y_pos_ = yp;
+	}
+
+	float get_x_pos() const {
+		return x_pos_;
+	}
+	float get_y_pos() const {
+		return y_pos_;
+	}
+	void SetMapXY(const int& mp_x, const int& mp_y) {
+		map_x_ = mp_x; map_y_ = mp_y;
+	}
+	void set_clips();
+	bool LoadImg(std::string path, SDL_Renderer* screen);
+	void Show(SDL_Renderer* des);
+	int get_width_frame() const {
+		return width_frame_;
+	}
+	int get_height_frame() const {
+		return height_frame_;
+	}
+	// Hành động cho bot
+	void DoPlayer(Map& gMap);
+	void CheckToMap(Map& gMap);
+
+
+	void InitThreats();
+
+	void set_type_move(const int& typeMove) { type_move_ = typeMove; }
+
+	
+
+	void set_input_left(const int& ipleft) { input_type_.left_ = ipleft; }
+	void ImpMoveType(SDL_Renderer* screen);
+
+
+	// bot tấn công
+
+	std::vector <BulletObject*> get_bullet_list() const { return bullet_list_; }
+	void set_bullet_list(const std::vector<BulletObject*>& bl_list) { bullet_list_ = bl_list; }
+
+	void InitBullet(BulletObject* p_bullet, SDL_Renderer* screen);
+	void MakeBullet(SDL_Renderer* screen, const int& x_limit, const int& y_limit);
+
+	void RemoveBullet(const int& idx);
+	void ktraN(const float& x, const float& y)
+	{
+		vt_x = x;
+		vt_y = y;
+	}
+
+private:
+
+	
+	float vt_x;
+	float vt_y;
+	int map_x_;
+	int map_y_;
+	float x_val_;
+	float y_val_;
+	float x_pos_;
+	float y_pos_;
+	bool on_ground_;
+	int come_back_time_;
+	SDL_Rect frame_clip_[THREAT_FRAME_NUM];
+	int width_frame_;
+	int height_frame_;
+	int frame_;
+
+	int type_move_;
+
+
+	Input input_type_;
+
+	std::vector<BulletObject*>bullet_list_;
+};
+
+
+
+
+#endif // !THREATS_OBJECT_H_
---
+++ SDL_Game/main.cpp
@@ -0,0 +1,195 @@
+﻿#include "stdafx.h"
+#include "CommonFunc.h"
+ #include "BaseObject.h"
+#include"game_map.h"
+#include "MainObject.h"
+#include "ImpTimer.h"
+#include  "ThreatsObject.h"
+
+
+
+#undef main
+BaseObject g_background;
+
+bool InitData() {
+
+	bool success = true;
+	int ret = SDL_Init(SDL_INIT_VIDEO);
+	if (ret < 0)  return false;
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+	g_window = SDL_CreateWindow("Ninja fight", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+
+	if (g_window == NULL) {
+
+		success = false;
+
+	}
+	else {
+		g_screen = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+		if (g_screen == NULL) success = false;
+
+		else {
+			SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+			int imgFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imgFlags) && imgFlags)) success = false;
+		}
+	}
+	return success;
+
+}
+
+
+bool LoadBackground() {
+	bool ret = g_background.LoadImg("img//newbkgr4.jpg", g_screen);
+	if (ret == false) return false;
+	return true;
+}
+void close() {
+	g_background.Free();
+
+
+	SDL_DestroyRenderer(g_screen);
+	g_screen = NULL;
+	SDL_DestroyWindow(g_window);
+	g_window = NULL;
+
+	IMG_Quit();
+	SDL_Quit();
+}
+// T?o m?y con bot
+
+
+
+int main(int arc, char* argv[]) 
+{
+
+	ImpTimer fps_timer;
+	float vtrix;
+	float vtriy;
+
+
+
+	if (InitData() == false) return -1;
+	if (LoadBackground() == false) return -1;
+
+	GameMap game_map;
+
+	char file_name[20] = "map/map01.dat";
+	game_map.LoadMap(file_name);
+
+	game_map.LoadTiles(g_screen);
+
+
+	MainObject p_player;
+	p_player.LoadImg("img//player1_right.png", g_screen);
+	p_player.set_clips();
+
+
+	ThreatsObject p_threat;
+	p_threat.LoadImg("img//threat_left.png",g_screen);
+	p_threat.set_clips();
+
+
+
+	bool is_quit = false;
+	while (!is_quit)
+	{
+		fps_timer.start();
+
+		while (SDL_PollEvent(&g_event) != 0) {
+
+
+			if (g_event.type == SDL_QUIT) {
+
+				is_quit = true;
+
+			}
+			p_player.HandelInputAction(g_event, g_screen);
+		}
+
+
+		SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+		SDL_RenderClear(g_screen);
+
+		g_background.Render(g_screen, NULL);
+
+
+
+
+		Map map_data = game_map.getMap();
+
+
+		vtrix = p_player.vitri_x();
+		vtriy = p_player.vitri_y();
+		p_player.HandleBullet(g_screen);
+
+		p_player.SetMapXY(map_data.start_x_, map_data.start_y_);
+
+		p_player.Doplayer(map_data);
+
+		p_player.Show(g_screen);
+
+		game_map.SetMap(map_data);
+
+		game_map.DrawMap(g_screen);
+
+
+
+		p_threat.SetMapXY(map_data.start_x_, map_data.start_y_);
+		p_threat.ktraN(vtrix, vtriy);
+		p_threat.ImpMoveType(g_screen);
+		p_threat.DoPlayer(map_data);
+		p_threat.MakeBullet(g_screen, SCREEN_WIDTH, SCREEN_HEIGHT);
+		p_threat.Show(g_screen);
+
+
+
+
+
+		// tạo va chạm của người và bot
+		std::vector<BulletObject*> bullet_arr = p_player.get_bullet_list();
+
+		for (int r = 0; r < bullet_arr.size(); r++)
+		{
+			BulletObject* p_bullet = bullet_arr.at(r);
+			if (p_bullet != NULL)
+			{
+
+
+				SDL_Rect tRect;
+				tRect.x = p_threat.GetRect().x;
+				tRect.y = p_threat.GetRect().y;
+				tRect.w = p_threat.get_width_frame();
+				tRect.h = p_threat.get_height_frame();
+
+				SDL_Rect bRect = p_bullet->GetRect();
+
+
+
+
+			}
+		}
+	
+
+
+
+
+	SDL_RenderPresent(g_screen);
+	int  real_imp_time = fps_timer.get_ticks();
+
+	int time_one_frame = 1000 / FRAME_PER_SECOND;
+
+	if (real_imp_time < time_one_frame)
+	{
+		int delay_time = time_one_frame - real_imp_time;
+		if (delay_time >= 0)
+			SDL_Delay(delay_time);
+	}
+}
+
+
+
+	close();
+	return 0;
+}
